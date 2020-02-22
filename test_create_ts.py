03c6e9e298652f5f25ed7c9cd6aa7fcf4863fae1
Andrew Chen <andrewchen5678@gmail.com>
2020-02-22 14:47:30 -0800

    test

---
+++ test_create_ts.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os, datetime
+import argparse
+from shlex import quote
+
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--directory', required=True)
+
+args = parser.parse_args()
+
+# Working directory
+directory = args.directory
+
+
+# Get a list of files in the directory
+filelist = os.listdir( directory )
+
+
+for file in filelist:
+    # Get the create time of the file, ONLY TESTED ON MAC
+    create_time = os.stat(os.path.join(directory,file)).st_birthtime
+    #print(create_time)
+    # get the readable timestamp format 
+    format_time = datetime.datetime.fromtimestamp( create_time )
+    
+    format_time_string = format_time.strftime("%Y-%m-%d %H:%M:%S")
+    print(file,' timestamp:',format_time_string)
