3d9fc7caac391b70ae0999e4863682cd69939eda
Andrew Chen <andrewchen5678@gmail.com>
2020-02-22 14:26:39 -0800

    improved with confirmation

--- rename_based_on_creation_date.py
+++ rename_based_on_creation_date.py
@@ -6,6 +6,40 @@ import os, datetime
 import argparse
 from shlex import quote
 
+import sys
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--directory', required=True)
 
@@ -15,10 +49,26 @@ args = parser.parse_args()
 directory = args.directory
 
 # extensions for files that we want rename
-extensions = (['.mp3']);
+extensions = (['.mp3'])
+
+
+# total files
+total = 0
+
+mp3s = []
 
 # Get a list of files in the directory
 filelist = os.listdir( directory )
+
+for filerel in filelist:
+    file = os.path.join(directory,filerel)
+    # split the file into filename and extension
+    filename, extension = os.path.splitext(filerel)
+    if ( extension.lower() in extensions ):
+        total = total + 1
+        mp3s.append([filename,extension,file])
+
+
 
 # New file dictionary
 newfilesDictionary = {}
@@ -26,43 +76,55 @@ newfilesDictionary = {}
 # count the number of files that are renamed
 count = 0
 
-for filerel in filelist:
-	file = os.path.join(directory,filerel)
-	# split the file into filename and extension
-	filename, extension = os.path.splitext(filerel)
-	# if the extension is a valid extension
-	if ( extension.lower() in extensions ):
-		# Get the create time of the file
-		create_time = os.stat(file).st_birthtime
-		print(create_time)
-		# get the readable timestamp format 
-		format_time = datetime.datetime.fromtimestamp( create_time )
-		# convert time into string
-		#format_time_string = format_time.strftime("%Y-%m-%d %H.%M.%S") # e.g. 2015-01-01 09.00.00.jpg
-		format_time_string = format_time.strftime("%Y-%m-%d_%H_%M_%S") # e.g. 2015-01-01 09.00.00.jpg
-		# Contruct the new name of the file
-		newfile = format_time_string + extension; 
+max_preview = 3
 
-		# If there is other files created at the same timestamp
-		if ( newfile in newfilesDictionary.keys() ):
-			index = newfilesDictionary[newfile] + 1;
-			newfilesDictionary[newfile] = index; 
-			newfile = format_time_string + '-' + str(index) + extension; # e.g. 2015-01-01 09.00.00-1.jpg
-		else:
-			newfilesDictionary[newfile] = 0; 
 
-		newfilefull = os.path.join(directory,newfile)
-		# rename the file
-		os.rename( file,  newfilefull);
+for filename, extension,file in mp3s[:max_preview]:
+    print(file)
 
-		command = 'id3v2 -D {}'.format(quote(newfilefull))
+if(total > max_preview):
+    print('...')
 
-		os.system(command)
+num_print = total if total < max_preview else max_preview
 
-		# count the number of files that are renamed
-		count = count + 1
-		# printing log
-		print( file.rjust(35) + '    =>    ' + newfile.ljust(35) )
+should_continue = query_yes_no(f"total number of mp3 files: {total}, type yes to confirm")
+
+
+if(not should_continue):
+    exit(1)
+
+for filename, extension,file in mp3s:
+    # Get the create time of the file, ONLY TESTED ON MAC
+    create_time = os.stat(file).st_birthtime
+    print(create_time)
+    # get the readable timestamp format 
+    format_time = datetime.datetime.fromtimestamp( create_time )
+    # convert time into string
+    #format_time_string = format_time.strftime("%Y-%m-%d %H.%M.%S") # e.g. 2015-01-01 09.00.00.jpg
+    format_time_string = format_time.strftime("%Y-%m-%d_%H_%M_%S") # e.g. 2015-01-01 09.00.00.jpg
+    # Contruct the new name of the file
+    newfile = format_time_string + extension
+
+    # If there is other files created at the same timestamp
+    if ( newfile in newfilesDictionary.keys() ):
+        index = newfilesDictionary[newfile] + 1
+        newfilesDictionary[newfile] = index; 
+        newfile = format_time_string + '-' + str(index) + extension; # e.g. 2015-01-01 09.00.00-1.jpg
+    else:
+        newfilesDictionary[newfile] = 0
+
+    newfilefull = os.path.join(directory,newfile)
+    # rename the file
+    os.rename( file,  newfilefull)
+
+    command = 'id3v2 -D {}'.format(quote(newfilefull))
+
+    os.system(command)
+
+    # count the number of files that are renamed
+    count = count + 1
+    # printing log
+    print( file.rjust(35) + '    =>    ' + newfile.ljust(35) )
 
 
 print( 'All done. ' + str(count) + ' files are renamed. ')
